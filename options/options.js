16dd466881c7ea9dd374e8f561cb1a9756fd881f
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-04-01 00:20:48 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "tst-daniel-g-undefi-73515",
-  url: "https://tst-daniel-g-undefi-73515.botics.co",
-  api: "https://tst-daniel-g-undefi-73515.botics.co/api/v1"
-}
+  "name": "tst-daniel-g-undefi-73515",
+  "url": "https://tst-daniel-g-undefi-73515.botics.co",
+  "api": "https://tst-daniel-g-undefi-73515.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
---
+++ screens/Untitled1/index.js
@@ -0,0 +1,32 @@
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
+import React from "react";
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled1 = () => {
+  const navigation = useNavigation();
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><Pressable onPress={() => {
+        navigation.navigate("Untitled2");
+      }}><View style={styles.oNTrQZtO}></View></Pressable></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  oNTrQZtO: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  }
+});
+export default Untitled1;
